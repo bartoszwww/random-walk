71ae8d8377f531d591a1900c6d8b325ff7a7624c
Bartek <bartek@procreative.eu>
2013-12-15 20:49:45 +0100

    ohlc support added + candlestick flot plugin

--- src/random-walk.js
+++ src/random-walk.js
@@ -1,13 +1,20 @@
 /**
- *
+ * Initial random walk algorythm was taken from
+ * http://stackoverflow.com/questions/11355615/simulating-stock-rates
  */
 function RandomWalk(config) {
 	this._c = {
 		start: 1,
-		stop: 5000,
+		stop: 100,
 		stepX: 1,
 		stepY: 1,
-		price: 0
+		price: 0,
+		ohlc: true,
+		ohlcStepsPerCycle: 10
+	}
+	// merge config
+	if (config) {
+		
 	}
 	
 	this.indicators = [];
@@ -17,10 +24,30 @@ function RandomWalk(config) {
 	this._initData();
 }
 RandomWalk.prototype._initData = function() {
+	var ohlc = [], min, max;
 	for (var i = this._c.start; i <= this._c.stop; i += this._c.stepX) {
-		this.values.push([i, this.currentPrice]);
-		this.currentPrice += Math.random() >= 0.5 ? this._c.stepY : -this._c.stepY;
+		if (this._c.ohlc) {
+			ohlc = [i, this.currentPrice]; // add open
+			min = max = this.currentPrice;
+			for (var j = 0; j < this._c.ohlcStepsPerCycle - 1; j++) {
+				this._generateNewPrice();
+				min = min > this.currentPrice ? this.currentPrice : min;
+				max = max < this.currentPrice ? this.currentPrice : max;
+			}
+			ohlc.push(this.currentPrice); // add close
+			ohlc.push(min); // add low
+			ohlc.push(max); // add high
+			this.values.push(ohlc);
+			this._generateNewPrice();
+		}
+		else {
+			this.values.push([i, this.currentPrice]);
+			this._generateNewPrice();
+		}
 	}
+}
+RandomWalk.prototype._generateNewPrice = function() {
+	this.currentPrice += Math.random() >= 0.5 ? this._c.stepY : -this._c.stepY;
 }
 RandomWalk.prototype.addIndicator = function(indicator) {
 	this.indicators.push(indicator);
